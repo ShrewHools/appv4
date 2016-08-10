0cae6c6d64ce9486a08ba20d4884213b114568cc
shrewhools <vladyukha@yandex.ru>
2016-08-10 21:24:27 +0300

    init

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @users = User.all
+    unless user_signed_in?
+      flash[:error]="Для просмотра всех пользователей требуется войти в существующий аккаунт, либо зарегистрироваться"
+      redirect_to root_path
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'static_pages#index'
+  # get 'static_pages/show', as:'users_root'
+  match '/users', to:'static_pages#show', via: 'get'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
