ebace84e5599da1fd8b4e3f8ec32f2101d046d98
shrewhools <vladyukha@yandex.ru>
2016-08-11 11:10:43 +0300

    Add devise controller + style btn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'static_pages/show', as:'users_root'
   match '/users', to:'static_pages#show', via: 'get'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions",
+                                    registrations: "users/registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
