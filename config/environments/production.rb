a1d052c747055102c87e5b3ac4c9de889585dd25
shrewhools <vladyukha@yandex.ru>
2016-08-16 02:11:05 +0300

    heroku d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.action_cable.url = "wss://salty-sea-24129.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://salty-sea-24129.herokuapp.com',
+                                               'http://salty-sea-24129.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: "users/sessions",
                                     registrations: "users/registrations"}
+
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
