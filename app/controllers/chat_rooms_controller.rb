662e5b584717749ad977813c9c437da26c289c2d
shrewhools <vladyukha@yandex.ru>
2016-08-16 23:01:12 +0300

    add access to chat

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,6 @@
 class ChatRoomsController < ApplicationController
+  before_action :access_to_chat, only: [:show]
+
   def index
     @chat_rooms = ChatRoom.all
     @chat_rooms_for_user = ChatRoom.where(chat_to_user: current_user.id)
@@ -30,4 +32,12 @@ class ChatRoomsController < ApplicationController
   def chat_room_params
     params.require(:chat_room).permit(:title, 'chat_to_user')
   end
+
+  def access_to_chat
+    unless current_user.chat_rooms.where("id=?", params[:id]).any? || ChatRoom.where(chat_to_user: current_user.id).where("id=?", params[:id]).any? || current_user.admin?
+      redirect_to root_path
+      flash[:danger]="У вас нет доступа к данному чату"
+    end
+  end
+
 end
